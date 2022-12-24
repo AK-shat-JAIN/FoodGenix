05e11e07f57729813e7900f24559037142ec557b
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2022-12-24 17:39:58 +0530

    Add files via upload

---
+++ Profile/js/Header.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+   {
+    document.getElementById("navbar").style.background = "white";
+    document.getElementById("navbar").style.color = "#77117b";
+    document.getElementById("navbar").style.height = "3rem";
+    document.getElementById("navbar").style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
+    document.getElementById("home").style.color = "#2081C3";
+    document.getElementById("cart-icon").style.color = "#2081C3";
+    document.getElementById("profile-icon").style.color = "#2081C3";
+    document.getElementById("logo").style.display = "block";
+    document.getElementById("box").style.display = "none";
+  }
+}
